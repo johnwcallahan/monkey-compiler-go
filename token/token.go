6d50f4bdccb41baa7a6e700a9c276e47dc99dc9a
johnwcallahan <johnwcallahan@gmail.com>
2019-05-22 22:19:02 -0400

    Initial lexer and tests (incomplete)

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// Type ...
+type Type string
+
+// Token ...
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+// Tokens ...
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
