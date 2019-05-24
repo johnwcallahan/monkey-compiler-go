b32808c39ebebc034b30f016c81edfb9ae7f691e
johnwcallahan <johnwcallahan@gmail.com>
2019-05-23 20:47:27 -0400

    LookupIdent function

--- token/token.go
+++ token/token.go
@@ -34,3 +34,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent ...
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
